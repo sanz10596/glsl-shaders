82ab921cb1846e3bc5972f69aa0f896d997a00e5
sanz10596 <Joses10596@gmail.com>
2016-12-26 23:13:11 -0700

    Update side-by-side-simple.glsl

--- misc/side-by-side-simple.glsl
+++ misc/side-by-side-simple.glsl
@@ -3,7 +3,7 @@
 #pragma parameter ana_zoom "Zoom" 0.75 -2.0 2.0 0.05
 #pragma parameter WIDTH "Side-by-Side Image Width" 3.05 1.0 7.0 0.05
 #pragma parameter HEIGHT "Side-by-Side Image Height" 2.0 1.0 5.0 0.1
-#pragma parameter BOTH "Horizontal Placement" 0.64 -2.0 2.0 0.005
+#pragma parameter BOTH "Horizontal Placement" 0.50 -2.0 2.0 0.005
 #pragma parameter warpX "warpX" 0.3 0.0 0.5 0.05
 #pragma parameter warpY "warpY" 0.3 0.0 0.5 0.05
 #ifdef PARAMETER_UNIFORM
